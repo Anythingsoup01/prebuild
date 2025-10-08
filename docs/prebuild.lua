9b71ff606939fe1e2b0ee0f4a0c3afef70dcfb2d
Anythingsoup01 <143284638+Anythingsoup01@users.noreply.github.com>
2025-10-08 17:10:31 -0400

    Updated documentation

--- docs/prebuild.lua
+++ docs/prebuild.lua
@@ -1,38 +1,43 @@
-// Note - Comments do not work at the moment
-workspace "docs-example"
-    defines
+Workspace = 
+{
+    name = "docs-example",
+    defines =
     {
         "ExampleDefinition1",
         "ExampleDefinition2",
         "ExampleDefinition3",
-    }
+    },
+}
 
-project "doc-inline-example1"
-    language "C++"    // C++ C
-    dialect "17"      // Depending on the language dialect will set c/c++ standard
-    kind "ConsoleApp" // ConsoleApp StaticLib SharedLib
+Project =
+{
+    name = "doc-inline-example1",
+    language = "C++",    // C++ C
+    dialect = "17",      // Depending on the language dialect will set c/c++ standard
+    kind = "ConsoleApp", // ConsoleApp StaticLib SharedLib
 
-    includedirs
+    includedirs =
     {
         "doc/example/include1",
         "doc/example/include2",
         "doc/example/include3",
-    }
+    },
 
-    files
+    files =
     {
         "doc/example/file1.cpp",
         "doc/example/file2.cpp",
         "doc/example/file3.cpp",
-        // or alternatively
+        # or alternatively
         "doc/*.cpp",
-    }
+    },
 
-    links
+    links =
     {
         "ExampleLib1",
         "ExampleLib2",
         "ExampleLib3"
-    }
+    },
+}
 
-external "external-folder"
+External = "external-folder"

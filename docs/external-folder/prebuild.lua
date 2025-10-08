734e000fbb91a87977738e2427afc559fcb5d289
Anythingsoup01 <143284638+Anythingsoup01@users.noreply.github.com>
2025-10-08 17:17:02 -0400

    Updated documentation

--- docs/external-folder/prebuild.lua
+++ docs/external-folder/prebuild.lua
@@ -1,25 +1,53 @@
-project "external-folder"
-    language "C++"    // C++ C
-    dialect "17"      // Depending on the language dialect will set c/c++ standard
-    kind "ConsoleApp" // ConsoleApp StaticLib SharedLib
+Project = 
+{
+    name = "external-folder",
+    language = "C++",    # C++ C
+    dialect = "17",      # Depending on the language dialect will set c/c++ standard
+    kind = "ConsoleApp", # ConsoleApp StaticLib SharedLib
 
-    includedirs
+    includedirs =
     {
         "doc/example/include1",
         "doc/example/include2",
         "doc/example/include3",
-    }
+    },
 
-    files
+    files =
     {
         "doc/example/file1.cpp",
         "doc/example/file2.cpp",
         "doc/example/file3.cpp",
-    }
+    },
 
-    links
+    links =
     {
         "ExampleLib1",
         "ExampleLib2",
         "ExampleLib3"
-    }
+    },
+
+    defines = 
+    {
+        "ExampleDefinition1",
+        "ExampleDefinition2",
+        "ExampleDefinition3"
+    },
+
+    filters =
+    {
+        {
+            name = "system:windows", # system:linux, configuration:<CONFIGURATIONNAME>
+            defines = "EXAMPLE_WINDOWS"
+
+            includedirs = "DIRECTORIES/TO/INCLUDE"
+            files = "FILES/TO/INCLUDE"
+        },
+        {
+            name = "system:linux", # system:linux, configuration:<CONFIGURATIONNAME>
+            defines = "EXAMPLE_LINUX"
+
+            includedirs = "DIRECTORIES/TO/INCLUDE"
+            files = "FILES/TO/INCLUDE"
+        }
+    },
+}
